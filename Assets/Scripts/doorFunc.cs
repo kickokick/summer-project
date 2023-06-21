f4e92f528a8b796329b347bfb6b3b7b39bf78c70
Poseidonspjs <103327899+Poseidonspjs@users.noreply.github.com>
2023-06-20 20:18:17 -0400

    Fixed door bug and now door closes

--- Assets/Scripts/doorFunc.cs
+++ Assets/Scripts/doorFunc.cs
@@ -5,14 +5,53 @@ using UnityEngine;
 public class doorFunc : MonoBehaviour
 {
     public Transform doorTransform;
-    private float x = 0;
-    void OnTriggerStay(Collider col)
+    private bool doorClosed = true;
+    private bool isTrigger = false;
+    //Creates flags instead
+    void OnTriggerEnter(Collider col)
     {
-        if ((col.tag == "Door") && (Input.GetKey("e")) && x == 0)
+        if(col.tag == "Door")
         {
-            doorTransform.position += new Vector3(5f, 0f, 0f);
-            print("SHITS workin");
-            x += 1;
+            isTrigger = true;
         }
     }
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Door")
+        {
+            isTrigger = false;
+        }
+    }
+    //Needed to be able to use GetKeyDown
+    void Update()
+    {
+        if ((isTrigger) && (Input.GetKeyDown("e")) && doorClosed)
+        {
+            print("Door opened");
+            doorTransform.position += new Vector3(5f, 0f, 0f);
+            doorClosed = false;
+        }
+        else if ((isTrigger) && (Input.GetKeyDown("e") && (!doorClosed)))
+        {
+            print("Door closed");
+            doorTransform.position += new Vector3(-5f, 0f, 0f);
+            doorClosed = true;
+        }
+    }
+    //Doesn't work
+/*     void OnTriggerStay(Collider col)
+    {
+        if ((col.tag == "Door") && (Input.GetKey("e")) && doorClosed == true)
+        {
+            doorTransform.position += new Vector3(5f, 0f, 0f);
+            print("Opened working");
+            doorClosed = false;
+        }
+        if ((col.tag == "Door") && (Input.GetKey("e")) && doorClosed == false)
+        {
+            doorTransform.position += new Vector3(-5f, 0f, 0f);
+            print("CLOSED Working");
+            doorClosed = true;
+        }
+    } */
 }
